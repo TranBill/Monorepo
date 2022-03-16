e254ddb030b878ed9bf8abe9fa4efab93fdc5d5a
Tran Loc <trandailoc060301@gmail.com>
2022-03-16 14:40:49 +0700

    fix expo

--- apps/example1/App.tsx
+++ apps/example1/App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import { Greeting, SharedButton} from '@bill/shared'
 import { StyleSheet, Text, View } from 'react-native';
-
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      {/* <Greeting/> */}
+      <SharedButton/>
     </View>
   );
 }
